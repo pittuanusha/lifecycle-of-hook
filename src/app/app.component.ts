33cf218c5b581815967a4ff9298bee9b342752be
Anusha Pittu <Anusha.Pittu@eliresearch.com>
2019-01-08 18:27:10 +0530

    taks on lifecycle of hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'example';
+  constructor() { }
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(){
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(){
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(){
+    console.log('ngOnDestory');
+  }
 }
+
